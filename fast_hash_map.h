6c3cfab721b8d76d2824e741b2d3cd4738b73448
Danil Mitasov <d_mitasov@mail.ru>
2025-02-02 14:00:46 +1000

    Добавлен powers_of_two, добавлены тесты

--- fast_hash_map.h
+++ fast_hash_map.h
@@ -5,6 +5,10 @@
 #include <algorithm>
 #include <vector>
 
+static const std::vector<unsigned long long int> primes{ 7, 13, 31, 61, 127, 251, 509, 1021, 2039, 4093, 8191, 16381, 32749, 65521, 131071, 262139, 524287, 1048573, 2097143, 4194301, 8388593, 16777213, 33554393, 67108859, 134217689, 268435399, 536870909, 1073741789, 2147483647, 4294967291, 8589934583, 17179869143, 34359738337, 68719476731, 137438953447, 274877906899, 549755813881, 1099511627689, 2199023255531, 4398046511093, 8796093022151, 17592186044399, 35184372088777, 70368744177643 };
+
+static const std::vector<unsigned long long int> powers_of_two{ 8, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096, 8192, 16384, 32768, 65536, 131072, 262144, 524288, 1048576, 2097152, 4194304, 8388608, 16777216, 33554432, 67108864, 134217728, 268435456, 536870912, 1073741824, 2147483648, 4294967296, 8589934592, 17179869184, 34359738368, 68719476736, 137438953472, 274877906944, 549755813888, 1099511627776, 2199023255552, 4398046511104, 8796093022208, 17592186044416, 35184372088832, 70368744177664 };
+
 
 template <typename Key, typename Value, typename Hash = std::hash<Key>>
 class fast_hash_map
@@ -17,32 +21,32 @@ class fast_hash_map
     };
 
 public:
-    fast_hash_map() : size_(0), index_(0), capacity_(primes_[index_]), arr_(new node[capacity_]{}) {}
+    fast_hash_map() : size_(0), index_(0), capacity_(primes[index_]), arr_(new node[powers_of_two[index_]]{}) {}
 
     fast_hash_map(const int capacity)
     {
         size_ = 0;
-        auto it = std::lower_bound(primes_.begin(), primes_.end(), capacity);
-        index_ = it == primes_.end() ? primes_.size() - 1 : std::distance(primes_.begin(), it); // проверить
-        capacity_ = primes_[index_];
-        arr_ = new node[capacity_]{};
+        auto it = std::lower_bound(primes.begin(), primes.end(), capacity);
+        index_ = it == primes.end() ? primes.size() - 1 : std::distance(primes.begin(), it); // проверить
+        capacity_ = primes[index_];
+        arr_ = new node[powers_of_two[index_]]{};
     }
 
     fast_hash_map(const std::initializer_list<std::pair<Key, Value>>& list)
     {
         size_ = 0;
-        auto it = std::lower_bound(primes_.begin(), primes_.end(), list.size() * 2);
-        index_ = it == primes_.end() ? primes_.size() - 1 : std::distance(primes_.begin(), it); // проверить
-        capacity_ = primes_[index_];
-        arr_ = new node[capacity_]{};
-        
+        auto it = std::lower_bound(primes.begin(), primes.end(), list.size() * 2);
+        index_ = it == primes.end() ? primes.size() - 1 : std::distance(primes.begin(), it); // проверить
+        capacity_ = primes[index_];
+        arr_ = new node[powers_of_two[index_]]{};
+
         for (const auto& element : list)
         {
             insert(element);
         }
     }
 
-    fast_hash_map(const fast_hash_map& other) : size_(0), index_(other.index_), capacity_(other.capacity_), arr_(new node[capacity_])
+    fast_hash_map(const fast_hash_map& other) : size_(0), index_(other.index_), capacity_(other.capacity_), arr_(new node[powers_of_two[index_]])
     {
         for (int i = 0; i < capacity_; ++i)
         {
@@ -69,8 +73,8 @@ public:
     {
         other.size_ = 0;
         other.index_ = 0;
-        other.capacity_ = primes_[other.index_];
-        other.arr_ = new node[other.capacity_];
+        other.capacity_ = primes[other.index_];
+        other.arr_ = new node[powers_of_two[other.index_]];
     }
 
     fast_hash_map& operator=(fast_hash_map&& other) noexcept
@@ -84,8 +88,8 @@ public:
             arr_ = other.arr_;
             other.size_ = 0;
             other.index_ = 0;
-            other.capacity_ = primes_[other.index_];
-            other.arr_ = new node[other.capacity_];
+            other.capacity_ = primes[other.index_];
+            other.arr_ = new node[powers_of_two[other.index_]];
         }
         return *this;
     }
@@ -152,8 +156,8 @@ public:
         delete[] arr_;
         size_ = 0;
         index_ = 0;
-        capacity_ = primes_[index_];
-        arr_ = new node[capacity_]{};
+        capacity_ = primes[index_];
+        arr_ = new node[powers_of_two[index_]]{};
     }
 
     ~fast_hash_map()
@@ -208,18 +212,18 @@ private:
     int capacity_;
     node* arr_;
     Hash hasher_;
-    inline static const std::vector<unsigned long long int> primes_{ 7, 13, 31, 61, 127, 251, 509, 1021, 2039, 4093, 8191, 16381, 32749, 65521, 131071, 262139, 524287, 1048573, 2097143, 4194301, 8388593, 16777213, 33554393, 67108859, 134217689, 268435399, 536870909, 1073741789, 2147483647, 4294967291, 8589934583, 17179869143, 34359738337, 68719476731 };
 
     // линейное пробирование
     int get_index(const Key& key) const
     {
         int index = hasher_(key) % capacity_;
+
         while (arr_[index].status != node::FREE)
         {
             if (arr_[index].status == node::OCCUPIED && arr_[index].element_.first == key)
                 return index;
 
-            index = (index + 1) % capacity_; // убрать операцию %
+            index = index == capacity_ - 1 ? 0 : index + 1;
         }
         return index;
     }
@@ -263,12 +267,12 @@ private:
 
     bool check_capacity()
     {
-        if (size_ > capacity_ / 2) // убрать операцию деления
+        if (size_ > capacity_ / 2)
         {
             int old_capacity = capacity_;
-            capacity_ = primes_[++index_];
+            capacity_ = primes[++index_];
             node* old_arr = arr_;
-            arr_ = new node[capacity_]{};
+            arr_ = new node[powers_of_two[index_]]{};
             size_ = 0;
 
             for (int i = 0; i < old_capacity; ++i)
--- test.cpp
+++ test.cpp
@@ -1,83 +1,181 @@
-﻿
+﻿#include <unordered_map>
 #include <cassert>
 #include <chrono>
 #include <random>
 #include "fast_hash_map.h"
 #include "flat_hash_map.hpp"
 
-void functional_test()
-{
-    fast_hash_map<int, std::string> map;
+//void functional_test()
+//{
+//    fast_hash_map<int, std::string> map;
+//
+//    // Тест вставки и доступа
+//    map.insert({ 1, "one" });
+//    map.insert({ 2, "two" });
+//    assert(map.contains(1) == true);
+//    assert(map.contains(2) == true);
+//    assert(map[1] == "one");
+//    assert(map[2] == "two");
+//
+//    // Тест обновления значения
+//    map[1] = "uno";
+//    assert(map[1] == "uno");
+//
+//    // Тест удаления
+//    map.erase(1);
+//    assert(map.contains(1) == false);
+//    assert(map.size() == 1); // должно остаться 1 элемент
+//
+//    // Тест очистки
+//    map.clear();
+//    assert(map.size() == 0);
+//    assert(map.contains(2) == false);
+//
+//    // Тест коллизий
+//    map.insert({ 3, "three" });
+//    map.insert({ 10, "ten" }); // Предположим, что 3 и 10 имеют одинаковый хэш
+//    assert(map.contains(3) == true);
+//    assert(map.contains(10) == true);
+//    assert(map[3] == "three");
+//    assert(map[10] == "ten");
+//
+//    // Тест повторного удаления
+//    map.erase(10);
+//    assert(map.contains(10) == false);
+//    assert(map.size() == 1); // должно остаться 1 элемент
+//
+//    for (int i = 0; i < 100; ++i)
+//    {
+//        map[i] = "hjbhbhbh";
+//    }
+//
+//    fast_hash_map<int, std::string> map2(map);
+//
+//    //map.print();
+//    //std::cout << "============" << std::endl;
+//    //map2.print();
+//
+//    assert(map2 == map);
+//
+//    fast_hash_map<int, std::string> map3 = std::move(map);
+//    assert(map2 == map3);
+//    assert(map2 != map);
+//
+//    //map3.print();
+//    //std::cout << map3.size() << " " << map3.capacity() << std::endl;
+//
+//    fast_hash_map<int, std::string> map4{ {1, "kbbjhb"}, {2,"kmklmkm"}, {4,"knljknjln"} };
+//    assert(map4.size() == 3);
+//    assert(map4.capacity() == 7);
+//    //map4.print();
+//    //std::cout << map3.size() << " " << map3.capacity() << std::endl;
+//
+//    std::cout << "Ok" << std::endl << "============" << std::endl;
+//}
 
-    // Тест вставки и доступа
-    map.insert({ 1, "one" });
-    map.insert({ 2, "two" });
-    assert(map.contains(1) == true);
-    assert(map.contains(2) == true);
-    assert(map[1] == "one");
-    assert(map[2] == "two");
+void functional_test() {
+    using Key = std::string;
+    using Value = int;
 
-    // Тест обновления значения
-    map[1] = "uno";
-    assert(map[1] == "uno");
+    // Конструктор по умолчанию
+    fast_hash_map<Key, Value> map1;
+    assert(map1.size() == 0);
+    assert(map1.capacity() > 0);
 
-    // Тест удаления
-    map.erase(1);
-    assert(map.contains(1) == false);
-    assert(map.size() == 1); // должно остаться 1 элемент
+    // Вставка элементов
+    map1.insert({ "one", 1 });
+    map1.insert({ "two", 2 });
+    map1.insert({ "three", 3 });
+    assert(map1.size() == 3);
+    assert(map1.contains("one"));
+    assert(map1.contains("two"));
+    assert(map1.contains("three"));
 
-    // Тест очистки
-    map.clear();
-    assert(map.size() == 0);
-    assert(map.contains(2) == false);
+    // Проверка оператора []
+    map1["four"] = 4;
+    assert(map1.size() == 4);
+    assert(map1["four"] == 4);
+    map1["four"] = 40;
+    assert(map1["four"] == 40);
 
-    // Тест коллизий
-    map.insert({ 3, "three" });
-    map.insert({ 10, "ten" }); // Предположим, что 3 и 10 имеют одинаковый хэш
-    assert(map.contains(3) == true);
-    assert(map.contains(10) == true);
-    assert(map[3] == "three");
-    assert(map[10] == "ten");
-
-    // Тест повторного удаления
-    map.erase(10);
-    assert(map.contains(10) == false);
-    assert(map.size() == 1); // должно остаться 1 элемент
-
-    for (int i = 0; i < 100; ++i)
-    {
-        map[i] = "hjbhbhbh";
+    // Проверка метода at
+    assert(map1.at("one") == 1);
+    assert(map1.at("two") == 2);
+    try {
+        map1.at("five"); // Должно вызывать исключение
+        assert(false);
+    }
+    catch (const std::out_of_range&) {
+        assert(true);
     }
 
-    fast_hash_map<int, std::string> map2(map);
+    // Удаление элементов
+    map1.erase("one");
+    assert(!map1.contains("one"));
+    assert(map1.size() == 3);
 
-    //map.print();
-    //std::cout << "============" << std::endl;
-    //map2.print();
+    // Проверка конструктора с начальной вместимостью
+    fast_hash_map<Key, Value> map2(100);
+    assert(map2.capacity() >= 100);
+    assert(map2.size() == 0);
 
-    assert(map2 == map);
+    // Конструктор копирования
+    fast_hash_map<Key, Value> map3 = map1;
+    assert(map3.size() == 3);
+    assert(map3.contains("two"));
+    assert(map3.contains("three"));
+    assert(map3.contains("four"));
 
-    fast_hash_map<int, std::string> map3 = std::move(map);
-    assert(map2 == map3);
-    assert(map2 != map);
-
-    //map3.print();
-    //std::cout << map3.size() << " " << map3.capacity() << std::endl;
-
-    fast_hash_map<int, std::string> map4{ {1, "kbbjhb"}, {2,"kmklmkm"}, {4,"knljknjln"} };
+    // Оператор присваивания
+    fast_hash_map<Key, Value> map4;
+    map4 = map1;
     assert(map4.size() == 3);
-    assert(map4.capacity() == 7);
-    //map4.print();
-    //std::cout << map3.size() << " " << map3.capacity() << std::endl;
+    assert(map4.contains("two"));
+    assert(map4.contains("three"));
+    assert(map4.contains("four"));
 
-    std::cout << "Ok" << std::endl << "============" << std::endl;
+    // Конструктор перемещения
+    fast_hash_map<Key, Value> map5 = std::move(map1);
+    assert(map5.size() == 3);
+    assert(map5.contains("two"));
+    assert(map5.contains("three"));
+    assert(map5.contains("four"));
+    assert(map1.size() == 0); // map1 должно быть пустым после перемещения
+
+    // Оператор присваивания с перемещением
+    fast_hash_map<Key, Value> map6;
+    map6 = std::move(map3);
+    assert(map6.size() == 3);
+    assert(map6.contains("two"));
+    assert(map6.contains("three"));
+    assert(map6.contains("four"));
+    assert(map3.size() == 0); // map3 должно быть пустым после перемещения
+
+    // Операторы сравнения
+    assert(map5 == map6);
+    map6.erase("two");
+    assert(map5 != map6);
+
+    // Проверка clear
+    map6.clear();
+    assert(map6.size() == 0);
+    assert(!map6.contains("three"));
+
+    // Тестирование краевых случаев
+    fast_hash_map<Key, Value> edge_map;
+    edge_map.insert({ "key", 1 });
+    edge_map.insert({ "key", 2 }); // Обновление значения
+    assert(edge_map.size() == 1);
+    assert(edge_map.at("key") == 2);
+
+    std::cout << "All tests passed!" << std::endl;
 }
 
 void test_find()
 {
 
-    fast_hash_map<int, int> myMap;
-    //std::unordered_map<int, int> myMap;
+    //fast_hash_map<int, int> myMap;
+    std::unordered_map<int, int> myMap;
     //ska::flat_hash_map<int, int> myMap;
     std::random_device rd;
     std::mt19937 gen(rd());
@@ -102,16 +200,16 @@ void test_find()
         // Измерение времени выполнения 1000000 операций поиска
         auto start = std::chrono::high_resolution_clock::now();
         for (int i = 0; i < 1000000; ++i) {
-            myMap.contains(i);
+            myMap.find(i);
         }
         auto end = std::chrono::high_resolution_clock::now();
 
         // Вычисление времени одной операции поиска
-        std::chrono::duration<double, std::nano> duration = end - start;
+        std::chrono::duration<int, std::nano> duration = end - start;
         double timePerOperation = duration.count() / 1000000; // Время на одну операцию в наносекундах
 
         // Вывод результата
-        std::cout << targetSize << " - " << timePerOperation << " ns" << std::endl;
+        std::cout << /*targetSize << " - " <<  */timePerOperation  /*<< " ns"  */<< std::endl;
 
         if (targetSize == maxSize) // 10000000
             return;
@@ -135,8 +233,8 @@ void test_find()
 
 void test_insert()
 {
-    fast_hash_map<int, int> myMap;
-    //std::unordered_map<int, int> myMap;
+    //fast_hash_map<int, int> myMap;
+    std::unordered_map<int, int> myMap;
     //ska::flat_hash_map<int, int> myMap;
     std::random_device rd;
     std::mt19937 gen(rd());
@@ -157,11 +255,11 @@ void test_insert()
         auto end = std::chrono::high_resolution_clock::now();
 
         // Вычисление времени одной операции поиска
-        std::chrono::duration<double, std::nano> duration = end - start;
+        std::chrono::duration<int, std::nano> duration = end - start;
         double timePerOperation = duration.count() / targetSize; // Время на одну операцию в наносекундах
 
         // Вывод результата
-        std::cout << targetSize << " - " << timePerOperation << " ns" << std::endl;
+        std::cout << /*targetSize << " - " <<  */timePerOperation  /*<< " ns"  */ << std::endl;
 
         if (targetSize == maxSize) // 1000000
             return;
@@ -180,8 +278,8 @@ void test_insert()
 
 void test_erase()
 {
-    fast_hash_map<int, int> myMap;
-    //std::unordered_map<int, int> myMap;
+    //fast_hash_map<int, int> myMap;
+    std::unordered_map<int, int> myMap;
     //ska::flat_hash_map<int, int> myMap;
     std::random_device rd;
     std::mt19937 gen(rd());
@@ -212,11 +310,11 @@ void test_erase()
         auto end = std::chrono::high_resolution_clock::now();
 
         // Вычисление времени одной операции поиска
-        std::chrono::duration<double, std::nano> duration = end - start;
+        std::chrono::duration<int, std::nano> duration = end - start;
         double timePerOperation = duration.count() / targetSize; // Время на одну операцию в наносекундах
 
         // Вывод результата
-        std::cout << targetSize << " - " << timePerOperation << " ns" << std::endl;
+        std::cout << /*targetSize << " - " <<  */timePerOperation  /*<< " ns"  */ << std::endl;
 
         if (targetSize == maxSize) // 1000000
             return;
@@ -235,8 +333,8 @@ void test_erase()
 
 int main()
 {
-    functional_test();
-    test_find();
+    //functional_test();
+    //test_find();
     //test_insert();
-    //test_erase();
+    test_erase();
 }
